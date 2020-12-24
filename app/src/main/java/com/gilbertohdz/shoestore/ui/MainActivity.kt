5be319bbfb85488b9a21d64ef75c30c9cf94fbc8
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2020-12-23 20:01:12 -0600

    feat: new navigation screen

--- app/src/main/java/com/gilbertohdz/shoestore/ui/MainActivity.kt
+++ app/src/main/java/com/gilbertohdz/shoestore/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gilbertohdz.shoestore
+package com.gilbertohdz.shoestore.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gilbertohdz.shoestore.R
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
